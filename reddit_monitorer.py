1e6ae92bf9d51c37fe30a8f380d5c99e4c703436
Callum <39635013+Zelatrix@users.noreply.github.com>
2020-02-17 16:26:48 +0000

    Refactored functionality into individual functions
    Extended functionality to constantly monitor the new posts in a given subreddit, and refactored so that each main piece of the program is handled by its own function.

--- reddit_monitorer.py
+++ reddit_monitorer.py
@@ -16,43 +16,89 @@ import praw
 import requests
 import random 
 import string 
+from pathlib import Path
+
 import os.path 
 from os import path
 
+# Creating a new Reddit instance to access the API
 reddit = praw.Reddit('reddit_bot')
 
-# Enter the name of a subreddit (case sensitive)
-# ex: pcmasterrace
-subR = input("Please enter the name of a subreddit: \n")
-subreddit = reddit.subreddit(subR)
+'''
+Allows for multiple subreddits to be entered. Enter each 
+subreddit followed by the enter key
+''' 
+def exec(n):
+    subR = ""
+    for i in range(n, 0, -1):
+        if not (i == 1):
+            inp = input(f"Please enter the names of {i} subreddits: \n")
+            subR += (inp + "+")
+        else:
+            inp = input(f"Please enter the name of {i} subreddit: \n")
+            subR += inp
+    print("Monitoring subreddits...")
+    return subR
 
-# submission = reddit.submission(id='f3pf6c')
-# print(submission.title)
-# comms = list(submission.comments)
-# pprint.pprint(comms[0].body)
-# pprint.pprint(submission.url)
-# print(dir(submission))
+'''
+Filtering out all the individual image types from the full
+list of extracted items. 
+'''
+def create_url_stream(subreddit):
+    # unfiltered_list = [sub.url for sub in subreddit.new(limit=5)] # subreddit.stream.submissions()]
+    # urls = []
+    # for sub in subreddit.new(limit=None): 
+    for sub in subreddit.stream.submissions():
+        # # only_jpgs = filter(lambda x: lambda x: x.endswith('.jpg'), subreddit.stream.submissions())
+        if (str(sub.url).endswith(('.jpg', '.jpeg', '.png'))):
+        # urls.append(sub.url)
+    # return urls
+            # return sub.url
+            # print(sub.url)
+            download_img(sub.url, '.jpg')
 
-unfiltered_list = [sub.url for sub in subreddit.new(limit=5)]
-jpgs =  list(filter(lambda x: x.endswith('.jpg'), unfiltered_list))
-jpegs = list(filter(lambda x: x.endswith('.jpeg'), unfiltered_list))
-pngs =  list(filter(lambda x: x.endswith('.png'), unfiltered_list))
+    # jpgs =  list(filter(lambda x: x.endswith('.jpg'), unfiltered_list))    # A list containing only *.jpg files
+    # jpegs = list(filter(lambda x: x.endswith('.jpeg'), unfiltered_list))   # A list containing only *.jpeg files
+    # pngs =  list(filter(lambda x: x.endswith('.png'), unfiltered_list))    # A list containing only *.png files
 
-urls = jpgs + jpegs + pngs
-choose_from = [i for i in string.ascii_letters] + [str(i) for i in range(10)]
-file_ext =  ".jpg"
+    # urls = jpgs + jpegs + pngs
+    # return urls
+    
+'''
+For this piece of code to function properly, a folder must be created 
+inside the same directory as the script to store the files in, as the 
+location that is used for output is a relative path
+'''
+def download_img(url, file_ext):
+    choose_from = [i for i in string.ascii_letters] + [str(i) for i in range(10)]
 
-for link in urls:
+    # for link in urls:
     rand_lst = [random.choice(choose_from) for _ in range(12)]
     rand_str = "".join(rand_lst)
+    location = Path.cwd()/f"output_pics"/f"{rand_str}{file_ext}"
 
-    r = requests.get(link)
-    if path.exists(f"{rand_str}{file_ext}") == False:
-        with open(f"{rand_str}{file_ext}", 'wb') as f:
+    # Send a request to the image URL that has been grabbed using the API
+    # If the filename that is to be assigned to a file does not already exist,
+    # create the file with that name, otherwise, skip that filename and generate
+    # a new one.
+    r = requests.get(url)
+    if path.exists(location) == False:
+        with open(location, 'wb') as f:
             f.write(r.content)
-    else:
-        continue
+            f.close()
+            print("Image saved!")
+    # else:
+        # continue
 
-# print(reddit.read_only)
-# for sub in reddit.subreddit('learnpython').hot(limit=10):
-#     print(sub.title)
+def main():
+    n = int(input("How many subreddits would you like to search? \n"))
+    subreddit = reddit.subreddit(exec(n))
+    
+    create_url_stream(subreddit)
+    # for img in urls:
+    #     download_img(img, '.jpg')
+    #     # download_img(img, '.jpeg')
+    #     # download_img(img, '.png')
+
+if __name__ == "__main__":
+    main()
